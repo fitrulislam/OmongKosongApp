3c181c64245e1fbe096ea3495aa85196aeb3d120
Fitrul Islam <fitrul.islam@gmail.com>
2018-03-27 16:40:21 +0700

    add routes

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.set('view engine','ejs')
 
 app.use(bodyParser.urlencoded({entended:false}))
 
+app.use('/',require('./routes'))
+
 app.listen(PORT,()=>{
   console.log(`OmongKosong running in ${PORT}`)
 })

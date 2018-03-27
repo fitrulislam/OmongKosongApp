6d10e120f357a3716123eead7dd0f6535f4bf42b
WikaSilo <wikanyaa@gmail.com>
2018-03-27 19:55:51 +0700

    user

--- models/user.js
+++ models/user.js
@@ -1,6 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
     name: DataTypes.STRING,
     username: DataTypes.STRING,
     password: DataTypes.STRING
@@ -8,5 +14,11 @@ module.exports = (sequelize, DataTypes) => {
   User.associate = function(models) {
     // associations can be defined here
   };
+
+  User.hook('beforeCreate', (user, options) => {
+    let nameRandom = ['bebek', 'kucing', 'burung', 'singa', 'anjing','ikan','onta','gajah','marmut','angsa','cicak','buaya','monyet']
+    user.name = nameRandom[Math.floor(Math.random()*nameRandom.length)];
+  });
+
   return User;
 };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 const routes = require('express').Router()
 
 routes.get('/',function(req,res){
-  res.render('home/home.ejs')
+  // res.render('home/home.ejs')
+  // res.send(req.session)
+  let obj= {
+    info: req.session
+  }
+  res.render('home/home.ejs', obj)
 })
+
+routes.use('/user', require('./user'))
 
 module.exports = routes

f6b70a402fb12294978c7668ad7e9d844fc6544d
Fitrul Islam <fitrul.islam@gmail.com>
2018-03-27 19:48:03 +0700

    add forum and comment

---
+++ migrations/20180327112012-create-comment.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ForumId: {
+        type: Sequelize.INTEGER
+      },
+      UserId: {
+        type: Sequelize.INTEGER
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      ParentId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Comments');
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 const routes = require('express').Router()
 
 routes.get('/',function(req,res){
-  res.render('home/home.ejs')
+  Forum.findAll().then(forums=>{
+    let obj = {
+      forums:forums
+    }
+    res.render('home/home.ejs',obj)
+  })
 })
+
+routes.use('/forum',require('./forum.js'))
 
 module.exports = routes

1f1c5245f97779e3ff0caca5b4164be52427d5e4
Fitrul Islam <fitrul.islam@gmail.com>
2018-03-28 12:41:49 +0700

    revisi dikit

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,17 @@
 const routes = require('express').Router()
+const {Forum} = require('../models')
 
 routes.get('/',function(req,res){
-
-  // res.render('home/home.ejs')
-  // res.send(req.session)
-  let obj= {
-    info: req.session
-  }
-  res.render('home/home.ejs', obj)
-})
-
-routes.use('/user', require('./user'))
-
   Forum.findAll().then(forums=>{
     let obj = {
-      forums:forums
+      forums: forums,
+      info: req.session
     }
     res.render('home/home.ejs',obj)
   })
 })
 
+routes.use('/user', require('./user'))
 routes.use('/forum',require('./forum.js'))
-
 
 module.exports = routes
